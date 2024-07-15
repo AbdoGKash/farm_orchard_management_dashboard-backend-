4196c21758dadb1049ef0da11ab8fde01e8bc891
Abdo Gamal Kashkoush <160290297+AbdoGKash@users.noreply.github.com>
2024-07-15 08:47:22 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,34 +1,4 @@
 <?php
-
-// $dsn = "pgsql:host=localhost;port=5432;;dbname=postgres";
-// $user = "postgres";
-// $pass = "abzo.123.com.*#@";
-// $option = array(
-//    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
-// );
-// $countrowinpage = 9;
-// try {
-//    $con = new PDO($dsn, $user, $pass, $option);
-//    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    header("Access-Control-Allow-Origin: *");
-//    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
-//    header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
-//    include "functions.php"; //include "../functions.php";
-// //    if (!isset($notAuth)) {
-//     //echo "Connected successfully to database.";
-
-// //    }
-// } catch (PDOException $e) {
-
-//    echo "Connection failed: " . $e->getMessage();
-// }
-
-
-
-
-
-
-
 $dbhost = 'localhost'; 
 $port = '5432';
 $dbname = 'postgres'; 
